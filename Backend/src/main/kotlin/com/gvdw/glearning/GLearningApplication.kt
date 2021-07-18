7fabdd1962c370c71637c269ac5726286864d307
Gullian <gullianvdwalt1@gmail.com>
2021-07-18 08:20:03 +0200

    init

---
+++ Backend/src/main/kotlin/com/gvdw/glearning/GLearningApplication.kt
@@ -0,0 +1,11 @@
+package com.gvdw.glearning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GLearningApplication
+
+fun main(args: Array<String>) {
+    runApplication<GLearningApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/com/gvdw/glearning/GLearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gvdw.glearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GLearningApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
