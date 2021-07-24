5c32170665f3b98742c52fef26147e1f8986cbe0
Gullian <gullianvdwalt1@gmail.com>
2021-07-24 13:55:19 +0200

    feat: user repository methods

--- backend/src/main/kotlin/com/gvdw/glearning/repositories/UsersRepository.kt
+++ backend/src/main/kotlin/com/gvdw/glearning/repositories/UsersRepository.kt
@@ -1,8 +1,16 @@
 package com.gvdw.glearning.repositories
 
+import com.gvdw.glearning.models.Users
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
 /**
  * @author Gullian Van Der Walt
  * Created at 11:28 on Jul, 2021
  */
-interface UsersRepository {
+
+interface UsersRepository : JpaRepository<Users, Long>{
+    override fun findById(id: Long): Optional<Users>
+    fun findByUsername(username: String): Users?
+    fun findByEmail(email: String): Users?
 }
