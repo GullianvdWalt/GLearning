a6ac29d2647a12b7268d5033b16112014d8f0b9c
Gullian <gullianvdwalt1@gmail.com>
2021-09-02 17:56:43 +0200

    feat: frontend, appbar, footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card height="400px">
+    <v-footer v-bind="localAttrs" padless>
+      <v-card flat tile width="100%" color="accent" class="text-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>G-Learning</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => ({
+    icons: [
+      'mdi-home',
+      'mdi-email',
+      'mdi-calendar',
+      'mdi-delete',
+    ],
+    padless: true,
+    variant: 'absolute',
+  }),
+  computed: {
+    localAttrs () {
+      const attrs = {};
+      attrs[this.variant] = true;
+      return attrs;
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,7 +1,27 @@
 import Vue from 'vue';
+
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:{
+        themes:{
+            light: {
+                primary: '#00A3FF',
+                secondary: '#716E6E',
+                accent: '#C4C4C4',
+                error: colors.red.accent3,
+                background: '#fff'
+            },
+            dark: {
+                primary: '#00A3FF',
+                secondary: '#716E6E',
+                accent: '#C4C4C4',
+                error: colors.red.accent3,
+                background: '#000'
+            },
+        }
+    }
 });
